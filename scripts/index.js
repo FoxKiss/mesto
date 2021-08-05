a1d5f765f6f24e536611a255222bf385dc391ac1
Arsentiy Polyakov <foxkiss1337@gmail.com>
2021-08-05 16:02:38 +0600

    feat: Добавить автоматическую загрузку карточек с помощью Js

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,13 @@
 const profileName = document.querySelector('.profile__title')
 const profileAbout = document.querySelector('.profile__subtitle')
 const profileEditButton = document.querySelector('.profile__edit-button');
+const profilePopup = document.querySelector('.popup__edit-profile');
 const profileInputName = profilePopup.querySelector('#profileName');
 const profileInputAbout = profilePopup.querySelector('#profileAbout');
-const profilePopup = document.querySelector('.popup__edit-profile');
+const profileForm = profilePopup.querySelector('#profileForm')
+
+//CARD
+const cardsList = document.querySelector('.cards__list')
 //POPUP BUTTON
 const profileCloseButton = profilePopup.querySelector('#profileCloseButton')
 
@@ -39,3 +43,21 @@ function handleEditProfile (evt) {
   closePopup(profilePopup)
 }
 profileForm.addEventListener('submit', handleEditProfile)
+
+
+
+//Создание Карточек
+function createCard(link, name) {
+  const cardTemplate = document.querySelector('#cardTemplate').content
+  const cardElement = cardTemplate.querySelector('.cards__list-element').cloneNode(true)
+  cardElement.querySelector('.cards__image').src= link
+  cardElement.querySelector('.cards__image').alt = name
+  cardElement.querySelector('.cards__name').textContent = name
+
+  cardsList.append(cardElement)
+  
+  
+}
+jsStartCards.forEach((item)=> {
+  createCard(item.link, item.name)
+})
---
+++ scripts/jsStartCards.js
@@ -0,0 +1,26 @@
+const jsStartCards= [
+  {
+    name: 'Афины',
+    link: './images/afin.jpg'
+  },
+  {
+    name: 'Делос',
+    link: './images/delos.jpg'
+  },
+  {
+    name: 'Дельфы',
+    link: './images/delph.jpg'
+  },
+  {
+    name: 'Крит',
+    link: './images/krit.jpg'
+  },
+  {
+    name: 'Микены',
+    link: './images/miken.jpg'
+  },
+  {
+    name: 'Мистрас',
+    link: './images/mistras.jpg'
+  }
+];
