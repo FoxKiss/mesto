0fc54cb6f63782ba3bd829bb10b94daec8c0d70f
Arsentiy Polyakov <foxkiss1337@gmail.com>
2021-09-06 03:03:31 +0600

    refactor: Фикс Автотестов

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ const profileName = document.querySelector('.profile__title');
 const profileAbout = document.querySelector('.profile__subtitle');
 const profileAvatar = document.querySelector('.profile__avatar')
 const profileEditButton = document.querySelector('.profile__edit-button');
-const profilePopup = document.querySelector('.popup__edit-profile');
+const profilePopup = document.querySelector('.popup_edit-profile');
 const profileForm = profilePopup.querySelector('#profileForm');
 const profileInputName = profileForm.querySelector('#profileName');
 const profileInputAbout = profileForm.querySelector('#profileAbout');
@@ -13,7 +13,7 @@ const profileInputAbout = profileForm.querySelector('#profileAbout');
 //CARD
 const cardsList = document.querySelector('.cards__list');
 const cardCreateButton = document.querySelector('.profile__add-button');
-const cardPopup = document.querySelector('.popup__create-cards');
+const cardPopup = document.querySelector('.popup_create-cards');
 const cardForm = cardPopup.querySelector('#cardForm');
 const cardInputName = cardForm.querySelector('#cardName');
 const cardInputLink = cardForm.querySelector('#cardLink');
@@ -21,17 +21,17 @@ const cardTemplate = document.querySelector('#cardTemplate')
 
 
 //IMAGE POPUP
-const imagesPopup = document.querySelector('.popup__images');
+const imagesPopup = document.querySelector('.popup_images');
 const cardPopImage = imagesPopup.querySelector('.popup__image');
 const cardPopImageName = imagesPopup.querySelector('.popup__image-name');
 
 //DELETE POPUP
-const deletePopup = document.querySelector('.popup__delete_card')
+const deletePopup = document.querySelector('.popup_delete_card')
 const deleteSubmitButton = document.querySelector('#deleteSubmitButton')
 
 //AVATAR POPUP
 const avatarPopup = document.querySelector('.popup__change-avatar')
-const avatarEditButton = document.querySelector('.profile__avatar_edit')
+const avatarEditButton = document.querySelector('.profile_avatar_edit')
 const avatarForm = document.querySelector('#avatarForm')
 
 
